27fbec6419fbda28a56d74dbd6bb3543ed15fb6f
matze45jub <elke-matthias@gmx.net>
2025-02-11 15:13:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("bestell-app-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css", // Falls vorhanden
+        "/script.js",  // Falls vorhanden
+        "/icon-192.png",
+        "/icon-512.png",
+      ]);
+    })
+  );
+});
 
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
